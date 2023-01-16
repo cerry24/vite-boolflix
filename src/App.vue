18159bfd4114d347042a764530379593ee2e590c
cerry24 <samuelecerretti@gmail.com>
2023-01-16 12:29:59 +0100

    implement API call & movie search

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script>
+import axios from 'axios';
+import { store } from './store.js';
+
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
@@ -6,13 +9,43 @@ export default {
     components: {
         AppHeader,
         AppMain
-    }
+    },
+
+    data() {
+        return {
+            store,
+            apiKey: '81966a6fcd70a879d3c6acdb9022aa91',
+            apiUri: 'https://api.themoviedb.org/3/search/movie'
+        }
+    },
+
+    methods: {
+        getMovies(inputQuery) {
+            axios.get(this.apiUri, {
+                params: {
+                    api_key: this.apiKey,
+                    query: inputQuery
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.store.movieList = response.data.results;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        this.getMovies();
+    },
 }
 </script>
 
 <template>
     <header>
-        <AppHeader />
+        <AppHeader @search="getMovies(store.searchedText)" />
     </header>
 
     <main>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,35 @@
 <script>
-export default {
+import { store } from '../store.js';
 
+export default {
+    name: 'AppHeader',
+
+    data() {
+        return {
+            store
+        }
+    },
 }
 </script>
 
 <template>
     <div>
+        <label for="user-searchbar">insert movie to search for</label>
+        <input type="text" name="user-searchbar" id="user-searchbar" v-model="store.searchedText">
+        <button @click="$emit('search')">search</button>
+    </div>
 
+    <div>
+        <ul>
+            <li v-for="movie in store.movieList">
+                <h3>{{ movie.title }}</h3>
+                <h5>{{ movie.original_title }}</h5>
+                <p>
+                    Language: {{ movie.original_language }}
+                    Rating: {{ movie.vote_average }}
+                </p>
+            </li>
+        </ul>
     </div>
 </template>
 
